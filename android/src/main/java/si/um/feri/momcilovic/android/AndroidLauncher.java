ecf594e478f843512cbe7bfb831d921bacbf7e3d
Laanmi <milan.momcilovic03@gmail.com>
2024-11-18 20:57:46 +0100

    naloga6.2-initial-project

---
+++ android/src/main/java/si/um/feri/momcilovic/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package si.um.feri.momcilovic.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import si.um.feri.momcilovic.MomcilovicBattleshipGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new MomcilovicBattleshipGame(), configuration);
+    }
+}
