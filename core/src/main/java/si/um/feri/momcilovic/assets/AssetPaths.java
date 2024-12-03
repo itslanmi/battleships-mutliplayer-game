3b142fd3952a10587ff56b13c06d71f7236c4534
Laanmi <milan.momcilovic03@gmail.com>
2024-12-03 10:30:06 +0100

    AssetPaths-updated

--- core/src/main/java/si/um/feri/momcilovic/assets/AssetPaths.java
+++ core/src/main/java/si/um/feri/momcilovic/assets/AssetPaths.java
@@ -4,8 +4,8 @@ public class AssetPaths {
 
     public static final String UI_FONT = "fonts/retro_gaming32.fnt";
     public static final String UI_SOUND_EXPLOSION = "sounds/explosion.wav";
-    public static final String UI_SOUND_DROP = "sounds/drop.mp3";
-    public static final String UI_MUSIC = "sounds/music.mp3";
+    public static final String UI_SOUND_DROP = "sounds/bomb_drop.wav";
+    public static final String UI_SOUND_MISS = "sounds/miss.wav";
     public static final String GAMEPLAY = "gameplay/gameplay.atlas";
 
     private AssetPaths() {
