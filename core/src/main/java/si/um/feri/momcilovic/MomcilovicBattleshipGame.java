333d6fa35504829e2b1c0a09ee267398c47db478
Laanmi <milan.momcilovic03@gmail.com>
2024-12-03 14:41:17 +0100

    AssetManager-all-assets-loaded

--- core/src/main/java/si/um/feri/momcilovic/MomcilovicBattleshipGame.java
+++ core/src/main/java/si/um/feri/momcilovic/MomcilovicBattleshipGame.java
@@ -2,20 +2,33 @@ package si.um.feri.momcilovic;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import si.um.feri.momcilovic.assets.AssetDescriptors;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MomcilovicBattleshipGame extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
 
+    private AssetManager assetManager;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+
+        assetManager = new AssetManager();
+        assetManager.load(AssetDescriptors.GAMEPLAY);
+        assetManager.load(AssetDescriptors.UI_FONT);
+        assetManager.load(AssetDescriptors.UI_SOUND_DROP);
+        assetManager.load(AssetDescriptors.UI_SOUND_EXPLOSION);
+        assetManager.load(AssetDescriptors.UI_SOUND_MISS);
     }
 
     @Override
